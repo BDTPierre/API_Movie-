7913ca5d5db159bfe80ef004a23479b253d52419
BDTPierre <buipierre@hotmail.fr>
2024-02-28 16:53:02 +0100

    v2

--- index.js
+++ index.js
@@ -6,21 +6,16 @@ let url = `https://api.themoviedb.org/3/search/movie?api_key=${ApiKey}&query=${s
 form.addEventListener('submit', e => e.preventDefault());
 
 let titre = document.querySelector('#title');
-
 titre.addEventListener('input', e => {
-    getData(e.target.value);
-    fetchMovies();
+    if (e.target.value.length > 2) {
+        search = e.target.value;
+        fetchMovies()
+    }
 })
 
-const getData = async (query) => {
-    if (query.length > 2) {
-        search = query;
-        return url = `https://api.themoviedb.org/3/search/movie?api_key=${ApiKey}&query=${search}&language=fr-FR`
-    }
-}
 
 const fetchMovies = async () => {
-    fetch(url)
+    await fetch(url)
         .then(response => response.json())
         .then(data => console.log(data))
         .catch(error => console.error(error));
