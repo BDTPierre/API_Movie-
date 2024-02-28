13f0014debe28b735638b5f33af2549c772c18a1
BDTPierre <buipierre@hotmail.fr>
2024-02-28 16:59:19 +0100

    v3

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 let form = document.querySelector('form');
+let input = document.querySelector('#title');
 let ApiKey = "5ae70743666449b1e8974bc0fcd234c2";
 let search = "";
-let url = `https://api.themoviedb.org/3/search/movie?api_key=${ApiKey}&query=${search}&language=fr-FR`;
 
 form.addEventListener('submit', e => e.preventDefault());
 
-let titre = document.querySelector('#title');
-titre.addEventListener('input', e => {
-    if (e.target.value.length > 2) {
-        search = e.target.value;
-        fetchMovies()
-    }
+input.addEventListener('input', e => {
+  if (e.target.value.length > 2) {
+    search = e.target.value;
+    fetchMovies()
+  }
 })
 
 
-const fetchMovies = async () => {
-    await fetch(url)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error(error));
+const fetchMovies = async() => {
+  await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${ApiKey}&query=${search}&language=fr-FR`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
 }
